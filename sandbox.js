ba5cc84793eb3a5302150d42ea2ad8a36c4b7744
Lalit Singh Shahi <shahilalit002@gmail.com>
2021-03-28 21:53:41 +0530

    types basics

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,29 @@
+// Lesson 1 - Compiling Typescript
 var character = "Lalit";
 console.log(character);
 var inputs = document.querySelectorAll("input");
 console.log(inputs);
 // tsc <ts filename>  ====> to compile the TS file into vanila JS
 // tsc <ts filename> -w  ====> to complie the TS file into vanila JS and watch for every change in the TS file
+// Lesson 2 - Types Basics
+var person = "Shahi";
+var age = 28;
+var isDevNoob = true;
+// Typescript does not allow to change the type of the variable once defined
+// person = 20; // This won't work - Typescript compiler will give error at compliling phase
+person = "FrontendBro"; // Variable's value can be change by the same type of value
+isDevNoob = false;
+// let countTotalCommits = (commits, days) => {
+//     console.log(commits * days)
+//     return commits * days;
+// }
+// Expects 2 args but given only 1, so issue is found and fixed at the compiling stage.
+// countTotalCommits("Shahi"); 
+// Does not show any error on compilation Until argument type is not defined.
+// countTotalCommits("shahi", 30); // Returns NaN
+var countTotalCommits = function (commits, days) {
+    console.log(commits * days);
+    return commits * days;
+};
+// countTotalCommits("shahi", 30); // Gives compilation error, argument expected of type number but provided as string.
+countTotalCommits(10, 30);
--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,5 @@
+// Lesson 1 - Compiling Typescript
+
 const character = "Lalit";
 
 console.log(character);
@@ -9,3 +11,39 @@ console.log(inputs);
 // tsc <ts filename>  ====> to compile the TS file into vanila JS
 
 // tsc <ts filename> -w  ====> to complie the TS file into vanila JS and watch for every change in the TS file
+
+// Lesson 2 - Types Basics
+
+let person = "Shahi";
+let age = 28;
+let isDevNoob = true;
+
+// Typescript does not allow to change the type of the variable once defined
+
+// person = 20; // This won't work - Typescript compiler will give error at compliling phase
+
+person = "FrontendBro";  // Variable's value can be change by the same type of value
+
+isDevNoob = false;
+
+// let countTotalCommits = (commits, days) => {
+//     console.log(commits * days)
+//     return commits * days;
+// }
+
+// Expects 2 args but given only 1, so issue is found and fixed at the compiling stage.
+// countTotalCommits("Shahi"); 
+
+// Does not show any error on compilation Until argument type is not defined.
+// countTotalCommits("shahi", 30); // Returns NaN
+
+
+
+let countTotalCommits = (commits: number, days: number) => {
+    console.log(commits * days)
+    return commits * days;
+}
+
+// countTotalCommits("shahi", 30); // Gives compilation error, argument expected of type number but provided as string.
+
+countTotalCommits(10, 30);
