2fb58f2c63eb7c02de84bfaeb562cc7f9a89fb29
Lalit Singh Shahi <shahilalit002@gmail.com>
2021-03-28 21:33:28 +0530

    compiling ts file and watching the changes

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var character = "Lalit";
+console.log(character);
+var inputs = document.querySelectorAll("input");
+console.log(inputs);
+// tsc <ts filename>  ====> to compile the TS file into vanila JS
+// tsc <ts filename> -w  ====> to complie the TS file into vanila JS and watch for every change in the TS file
---
+++ sandbox.ts
@@ -0,0 +1,11 @@
+const character = "Lalit";
+
+console.log(character);
+
+const inputs = document.querySelectorAll("input");
+
+console.log(inputs);
+
+// tsc <ts filename>  ====> to compile the TS file into vanila JS
+
+// tsc <ts filename> -w  ====> to complie the TS file into vanila JS and watch for every change in the TS file
